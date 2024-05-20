8f5f0f74cfb37cb3798af50de9b5dcbf9b95bf20
obre-bjorn <obre.bjorn@gmail.com>
2024-05-20 18:10:46 +0300

    created POST controllers and entity create forms

--- routes/catalog.js
+++ routes/catalog.js
@@ -36,6 +36,9 @@ router.get("/book/:id", book_controller.book_detail);
 // GET request for list of all Book items.
 router.get("/books", book_controller.book_list);
 
+
+
+
 /// AUTHOR ROUTES ///
 
 // GET request for creating Author. NOTE This must come before route for id (i.e. display author).
@@ -62,6 +65,9 @@ router.get("/author/:id", author_controller.author_detail);
 // GET request for list of all Authors.
 router.get("/authors", author_controller.author_list);
 
+
+
+
 /// GENRE ROUTES ///
 
 // GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
@@ -87,6 +93,9 @@ router.get("/genre/:id", genre_controller.genre_detail);
 
 // GET request for list of all Genre.
 router.get("/genres", genre_controller.genre_list);
+
+
+
 
 /// BOOKINSTANCE ROUTES ///
 
