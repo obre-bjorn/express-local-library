a73cbff2062824cf806af4746b84cac2b4b4c7e1
obre-bjorn <obre.bjorn@gmail.com>
2024-05-22 13:07:11 +0300

    implemented delete feature for all entities

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -86,11 +86,45 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+   const [genre,allBooksWithGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({genre: req.params.id},"title summary").exec()
+   ])
+
+   if(genre === null){
+    res.redirect("catalog/genres")
+   }
+
+   res.render("genre_delete",{
+    title:"Delete Genre",
+    genre,
+    genre_books: allBooksWithGenre
+   })
+
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
+  const [genre, allBooksWithGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({genre: req.params.id}, "title summary").exec()
+  ])
+
+  if(allBooksWithGenre.length > 0){
+    res.render("genre_delete",{
+      title: "Delete Genre",
+      genre,
+      genre_books: allBooksWithGenre
+    })
+  }else{
+    await Genre.findByIdAndDelete(req.body.genreid)
+    res.redirect("/catalog/genres")
+  }
+
+  
+
+
+
   res.send("NOT IMPLEMENTED: Genre delete POST");
 });
 
